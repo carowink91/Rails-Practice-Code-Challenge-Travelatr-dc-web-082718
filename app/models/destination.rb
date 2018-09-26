88d7f3697a7a68a90e388cc7d25533049274d9af
Angelica Winkler <carowink3@gmail.com>
2018-09-26 14:42:24 -0400

    filled out Blogger controller

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,8 @@
 class Destination < ApplicationRecord
+  has_many :posts
+  has_many :bloggers, through: :posts
 
+  def most_recent_posts
+    self.posts.order("created_at desc").limit(5)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'destinations/show'
+
+  get 'destinations/index'
+
+  get 'bloggers/index'
+
+  get 'bloggers/new'
+
+  get 'bloggers/create'
+
+  get 'bloggers/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bloggers, only: [:new, :create, :show, :index]
+
+  resources :destinations, only: [:show, :index]
+
+  resources :posts, except: [:delete]
 end
